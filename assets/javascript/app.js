73171bb0ca8d870a0fa0549a03af7290cb27d07f
Preston Ross <prestonross77@gmail.com>
2017-09-30 10:15:16 -0400

    site files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,23 @@
 // API Key= 2d659f708f2041ec9eb3b2723e66ea7d
+var = articleHTML;
+
+function articleSearch () {
+	articleHTML = 
+	
+`<header>
+
+	<h2>
+	 	
+	Top Articles
+	 		
+	<i class="fa fa-table" aria-hidden="true"></i>
+	 	
+	</h2> 
+	 	
+</header>
+
+<div class="articles"></div>`;
+
+$("search-results").html(articleSearch);
+
+}
