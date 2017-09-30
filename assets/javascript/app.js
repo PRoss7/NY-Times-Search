28ac81ac578360fbcbc28717f7111e9ee5404f86
Preston Ross <prestonross77@gmail.com>
2017-09-30 13:47:15 -0400

    site files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,65 @@
 // API Key= 2d659f708f2041ec9eb3b2723e66ea7d
-var = articleHTML;
+/*$(document).ready(function () {
 
-function articleSearch () {
-	articleHTML = 
 	
-`<header>
 
-	<h2>
-	 	
-	Top Articles
-	 		
-	<i class="fa fa-table" aria-hidden="true"></i>
-	 	
-	</h2> 
-	 	
-</header>
+	function articleSearch () {
+		
+		articleHTML = "<div class='article-section'>" + "<header class='article-header'>" + "<h2>" + "<i class='fa fa-table' aria-hidden='true'>" + "</i>" + "Top Articles" + "</h2>" + "</header>" + "</div>";
 
-<div class="articles"></div>`;
+		$(".search-results").html(articleHTML);
 
-$("search-results").html(articleSearch);
+	}
+
+
+	$(".search-results").on("click", ".search", function (){
+		
+		articleSearch();
+
+	});
+
+});
+
+var articleHTML;*/
+
+var APIKey= "2d659f708f2041ec9eb3b2723e66ea7";
+
+var search = $("#searchfor").val();
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=2d659f708f2041ec9eb3b2723e66ea7d&q="+ search;
+
+var articlesSet = 0;
+
+var results = 0;
+
+var startYear = 0;
+var endYear = 0;
+
+$("#search").on("click", function(event) {
+
+function runQuery(numArticles, queryURL) {
+
+
+$.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+
+        	console.log(queryURL);
+        	console.log(response);
+
+        $(".headline").text(response.response.docs["0"].headline.print_headline);
+        $(".author").text(response.response.docs["0"].byline);
+        $(".date").text(response.response.docs["0"].pub_date);
+        $(".website").text(response.response.docs["0"].queryURL);
+
+});
+      for (var i = 0; i < numArticles; i++) {
+
+      	articles++;
+
+      }
 
 }
+
+});
